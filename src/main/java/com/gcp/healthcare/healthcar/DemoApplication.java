c277d06bc5a6a273c6ffb56840c52e22dfc337f1
paragomni <parag@omnicuris.com>
2020-02-07 19:30:15 +0530

    Initial commit to set up project

---
+++ src/main/java/com/gcp/healthcare/healthcar/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.gcp.healthcare.healthcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+}
---
+++ src/main/java/com/gcp/healthcare/restController/SampleRestController.java
@@ -0,0 +1,11 @@
+package com.gcp.healthcare.restController;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sample")
+public class SampleRestController {
+
+}
